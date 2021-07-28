821f51bcf7427232ec0fa36887e0b7cb46315d76
Aditya Jadhav <39948420+Adityaaj18@users.noreply.github.com>
2021-07-28 19:47:26 +0530

    Day 15,16 completed

---
+++ Day 15/index.js
@@ -0,0 +1,13 @@
+class HelloWorld extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return React.createElement("h1", {}, "Hello React");
+  }
+}
+
+const root = document.getElementById(`root`);
+const element = React.createElement(HelloWorld);
+ReactDOM.render(element, root);
